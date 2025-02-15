f7bd759f7b824a8d3564a05eb5bb913f97b9d878
JlordS32 <jlordsty128@gmail.com>
2025-02-15 11:49:44 +1100

    decomposed

---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+import Button from "./Button";
+
+const Hero = () => {
+   return (
+      <section className="hero">
+         <div className="hero-img">
+            <div className="hero-bg">
+               <span>
+                  Modern <br />
+                  Art Gallery
+               </span>
+            </div>
+            <div className="image-container">
+               <span>
+                  Modern <br />
+                  Art Gallery
+               </span>
+            </div>
+         </div>
+         <div className="w-[450px] absolute top-[18rem] right-[10rem]">
+            <p className="pb-[3rem] text-base">
+               The arts in the collection of the Modern Art Gallery all started
+               from a spark of inspiration. Will these pieces inspire you? Visit
+               us and find out.
+            </p>
+            <Button text="Our Location" type="primary" />
+         </div>
+      </section>
+   );
+};
+
+export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,9 @@
-import Button from "../components/Button";
+import Hero from "../components/Hero";
 
 const Home = () => {
    return (
       <main>
-         <section className="hero">
-            <div className="hero-img">
-               <div className="hero-bg">
-                  <span>
-                     Modern <br />
-                     Art Gallery
-                  </span>
-               </div>
-               <div className="image-container">
-                  <span>
-                     Modern <br />
-                     Art Gallery
-                  </span>
-               </div>
-            </div>
-            <div className="w-[450px] absolute top-[18rem] right-[10rem]">
-               <p className="pb-[3rem] text-base">
-                  The arts in the collection of the Modern Art Gallery all
-                  started from a spark of inspiration. Will these pieces inspire
-                  you? Visit us and find out.
-               </p>
-               <Button text="Our Location" type="primary" />
-            </div>
-         </section>
+         <Hero />
       </main>
    );
 };
